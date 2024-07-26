341fabe19dad5b9bc5fe1e29dd28bccb3fc465eb
lexnovic <56923204+lexnovic@users.noreply.github.com>
2024-07-26 17:13:21 +0200

    Update netdog.py
    Corrected multiple errors in the code

--- netdog.py
+++ netdog.py
@@ -13,44 +13,47 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd),
                                      stderr=subprocess.STDOUT)
     return output.decode()
-    class NetDog:
-        def __init__(self, args, buffer=None):
-            self_args = args
-            self.buffer = buffer
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            def run(self):
-                if self.args.listen:
-                    self.listen()
-                else:
-                    self.send()
-        def send(self):
-            self.socket.connect((self.args.target, self.args.port))
-            if self.buffer:
-                self.socket.send(self.buffer)
 
-            try:
-                while True:
-                    recv_len = 1
-                    response = ''
-                    while recv_len:
-                        data = self.socket.recv(4096)
-                        recv_len = len(data)
-                        response += data.decode()
-                        if recv_len < 4096:
-                            break
-                        if response:
-                            print(response)
-                            buffer = input('> ')
-                            buffer += '\n'
-                            self.socket.send(buffer.encode())
-            except KeyboardInterrupt:
-                print('Session terminated.')
-                self.socket.close()
-                sys.exit()
+class NetDog:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('Session terminated.')
+            self.socket.close()
+            sys.exit()
 
     def listen(self):
-        self.socket.bind((self.args.target, aelf.args.port))
+        self.socket.bind((self.args.target, self.args.port))
         self.socket.listen(5)
         while True:
             client_socket, _ = self.socket.accept()
@@ -84,7 +87,7 @@ def execute(cmd):
                 try:
                     client_socket.send(b'NetDog: #> ')
                     while '\n' not in cmd_buffer.decode():
-                        cmd_buffer += client.socket.recv(64)
+                        cmd_buffer += client_socket.recv(64)
                     response = execute(cmd_buffer.decode())
                     if response:
                         client_socket.send(response.encode())
@@ -101,12 +104,12 @@ if __name__ == '__main__':
         epilog=textwrap.dedent('''Examples:
             netdog.py -t <IP_ADDRESS> -p <PORT> -l -c # command shell
             netdog.py -t <IP_ADDRESS> -p <PORT> -l -u=mytest.txt # upload to file
-            netdog.py -t <IP_ADDRESS> -p <PORT> -l -e=\"cat /etc/passwd" # executable command
+            netdog.py -t <IP_ADDRESS> -p <PORT> -l -e="cat /etc/passwd" # execute command
             echo 'ABC' | ./netdog.py -t <IP_ADDRESS> -p <PORT> # echo text to server port
             netdog.py -t <IP_ADDRESS> -p <PORT> # connect to server'''))
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l', '--execute', action='store_ture', help='listen')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
     parser.add_argument('-p', '--port', type=int, default=5051, help='specify port')
     parser.add_argument('-t', '--target', default='192.168.1.107', help='specify IP')
     parser.add_argument('-u', '--upload', help='upload file')
