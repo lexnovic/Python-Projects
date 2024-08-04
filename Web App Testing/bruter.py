2fe91f3ce5740ccb62d64fce05d0453fa4edde9e
Lexnovic <lexnovic@gmail.com>
2024-08-04 20:24:03 +0200

    directory bruter

---
+++ Web App Testing/bruter.py
@@ -0,0 +1,65 @@
+import queue
+import requests
+import threading
+import sys
+
+AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
+EXTENSIONS = ['.php', '.bak', '.orig', '.inc']
+TARGET = "http://changethis.com"
+THREADS = 50
+WORDLIST = ""
+
+
+def get_words(resume=None):
+    def extend_words(word):
+        if "." in word:
+            words.put(f'/{word}')
+        else:
+            words.put(f'/{word}')
+
+        for extension in EXTENSIONS:
+            words.put(f'/{word}{extension}')
+
+    with open(WORDLIST) as f:
+        raw_word = f.read()
+
+    found_resume = False
+    word = queue.Queue()
+    for word in raw_word.split():
+        if resume is not None:
+            if found_resume:
+                extend_words(word)
+            elif word == resume:
+                found_resume = True
+                print(f'Resuming wordlist from: {resume}')
+        else:
+            print()
+            extend_words(word)
+    return words
+
+
+def dir_bruter(words):
+    headers = {'User-Agent' : AGENT}
+    while not words.empty():
+        url = f'{TARGET}{words.get()}'
+        try:
+            r = requests.get(url, headers=headers)
+        except requests.exceptions.ConnectionError:
+            sys.stderr.write('x');sys.stderr.flush()
+            continue
+
+        if r.status_code == 200:
+            print(f'\nSuccess ({r.status_code}: {url})')
+        elif r.status_code == 404:
+            sys.stderr.write('.');sys.stderr.flush()
+        else:
+            print(f'{r.status_code} => {url}')
+
+
+if __name__ == __main__:
+    words = get_words()
+    print('Press return to continue.')
+    sys.stdin.readline()
+    for _ in range(THREADS):
+        t = threading.Thread(target=dir_bruter, args=(words,))
+        t.start()
