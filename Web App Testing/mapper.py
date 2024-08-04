215acd7d101aa20ef068e2bd7dc55d7b59f48665
Lexnovic <lexnovic@gmail.com>
2024-08-04 17:09:33 +0200

    file mapper: add test_remote and run func, main func refactoring

--- Web App Testing/mapper.py
+++ Web App Testing/mapper.py
@@ -20,9 +20,36 @@ def gather_paths():
                 continue
             path = os.path.join(root, fname)
             if path.startswith('.'):
-                path = [:1]
+                path = [1:]
             print(path)
             web_paths.put(path)
+
+
+def test_remote():
+    while not web_paths.empty():
+        path: object = web_paths.get()
+        url = f'{TARGET}{path}'
+        time.sleep(2)
+        r = requests(url)
+        if r.status_code == 200:
+            answers.put(url)
+            sys.stdout.write('+')
+        else:
+            sys.stdout.write('x')
+        sys.stdout.flush()
+
+
+def run():
+    mythreads = list()
+    for i in range(THREADS):
+        print(f'Spawning thread {i}')
+        t = threading.Thread(target=test_remote)
+        mythreads.append(t)
+        t.start()
+
+    for thread in mythreads:
+        thread.join()
+
 
 @contextlib.contextmanager
 def chdir(path):
@@ -38,7 +65,13 @@ def chdir(path):
         os.chdir(this_dir)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     with chdir("/home/tim/Downloads/wordpress"):
         gather_paths()
     input('Press return to continue.')
+
+    run()
+    with open('myanswers.txt', 'w') as f:
+        while not answers.empty():
+            f.write(f'{answers.get()}\n')
+    print('done')
