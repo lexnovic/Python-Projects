82ce6213d396eae77f66b0da89e587b6f8672b28
Lexnovic <lexnovic@gmail.com>
2024-09-16 18:28:02 +0200

    http server add gzip compression

--- http_server.py
+++ http_server.py
@@ -1,116 +1,122 @@
-import socket
-import threading
+import asyncio
 import argparse
-import os
+import re
+import sys
+from asyncio.streams import StreamReader, StreamWriter
+from pathlib import Path
+import gzip
+import io
+import binascii
 
-FILE_DIR = ""
+GLOBALS = {}
 
-def create_headers(headers: dict):
-    return "\r\n".join([f"{k}: {v}" for k, v in headers.items()])
+def stderr(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
-def create_response(client, status: str, headers: dict, body: bytes):
-    resp = f"HTTP/1.1 {status}\r\n"
-    if len(headers) > 0:
-        resp += create_headers(headers) + "\r\n"
-    resp += "\r\n"
-    if len(body) > 0:
-        resp += body.decode()
-    client.send(resp.encode())
-    client.close()
+def gzip_compress(data: str) -> bytes:
+    byte_data = data.encode('utf-8')
+    compressed_data = io.BytesIO()
+    with gzip.GzipFile(fileobj=compressed_data, mode='wb') as gzip_file:
+        gzip_file.write(byte_data)
+    return compressed_data.getvalue()
 
-    return
+def parse_request(content: bytes) -> tuple[str, str, dict[str, str], str]:
+    first_line, *tail = content.split(b"\r\n")
+    method, path, _ = first_line.split(b" ")
+    headers: dict[str, str] = {}
+    while (line := tail.pop(0)) != b"":
+        key, value = line.split(b": ")
+        headers[key.decode()] = value.decode()
+    return method.decode(), path.decode(), headers, b"".join(tail).decode()
 
-def extract_headers(data):
-    values = {}
-    for d in data:
-        k = d.split(":")[0]
-        v = d[len(k) + 2 :]
-        v = v.replace("\r\n", "")
-        values[k] = v
-    return values
+def make_response(
+    status: int,
+    headers: dict[str, str] | None = None,
+    body: bytes | str = b"",
+) -> bytes:
+    headers = headers or {}
+    msg = {
+        200: "OK",
+        201: "Created",
+        404: "Not Found",
+    }
 
-def handle_client(client):
-    global FILE_DIR
-    data = client.recv(1024)
-    if b"\r\n\r\n" not in data:
-        client.close()
-        return
+    if isinstance(body, str):
+        body = body.encode('utf-8')  # Convert string body to bytes if needed
 
-    header_data = data[: data.find(b"\r\n\r\n")].decode().split("\r\n")
-    body_data = data[data.find(b"\r\n\r\n") + 4 :]
-    headers = extract_headers(header_data[1:])
-    path_data = header_data[0].split(" ")
+    response_headers = [
+        f"HTTP/1.1 {status} {msg[status]}",
+        *[f"{k}: {v}" for k, v in headers.items()],
+        f"Content-Length: {len(body)}",
+        "",
+        ""
+    ]
 
-    path_data = header_data[0].split(" ")
-    path_type = path_data[0]
-    path_path = path_data[1]
-    path_http = path_data[2]
+    return "\r\n".join(response_headers).encode('utf-8') + body
 
-    if path_path == "/":
-        client.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        client.close()
-    else:
-        if "/files/" in path_path:
-            file = path_path[path_path.find("/files/") + 7 :]
-            p = f"{FILE_DIR}{file}"
-            if path_type == "GET":
-                if not os.path.exists(p):
-                    client.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-                    client.close()
-                    return
+async def handle_connection(reader: StreamReader, writer: StreamWriter) -> None:
+    method, path, headers, body = parse_request(await reader.read(2**16))
 
-                contents = open(p, "rb").read()
-                return create_response(
-                    client,
-                    "200 OK",
-                    {
-                        "Content-Type": "application/octet-stream",
-                        "Content-Length": len(contents),
-                    },
-                    contents,
+    # Check if the client accepts gzip encoding
+    accept_encoding = headers.get("Accept-Encoding", ", ")
+    use_gzip = "gzip" in accept_encoding.lower()
+
+    response_headers = {}
+    if use_gzip:
+        response_headers["Content-Encoding"] = "gzip"
+
+    if re.fullmatch(r"/", path):
+        writer.write(b"HTTP/1.1 200 OK\r\n\r\n")
+        stderr(f"[OUT] /")
+    elif re.fullmatch(r"/user-agent", path):
+        ua = headers["User-Agent"]
+        writer.write(make_response(200, {"Content-Type": "text/plain", **response_headers}, ua))
+        stderr(f"[OUT] user-agent {ua}")
+    elif match := re.fullmatch(r"/echo/(.+)", path):
+        msg = match.group(1)
+        if use_gzip:
+            gzip_msg = gzip_compress(msg)
+            writer.write(make_response(200, {"Content-Type": "text/plain", **response_headers}, gzip_msg))
+            stderr(f"[OUT] echo {gzip_msg}")
+        else:
+            writer.write(make_response(200, {"Content-Type": "text/plain", **response_headers}, msg))
+            stderr(f"[OUT] echo {msg}")
+    elif match := re.fullmatch(r"/files/(.+)", path):
+        p = Path(GLOBALS["DIR"]) / match.group(1)
+        if method.upper() == "GET" and p.is_file():
+            writer.write(
+                make_response(
+                    200,
+                    {"Content-Type": "application/octet-stream", **response_headers},
+                    p.read_text(),
                 )
-    if path_type == "POST":
-        with open(p, "wb") as out:
-            out.write(body_data)
-        client.send("HTTP/1.1 201 Created\r\n\r\n".encode())
-        client.close()
-        return
-
-    if "/echo/" in path_path:
-        echo = path_path[path_path.find("/echo/") + 6 :]
-        return create_response(
-                client,
-                "200 OK",
-                {"Content-Type": "text/plain", "Content-Length": len(echo)},
-                echo.encode(),
             )
-    if "/user-agent" in path_path:
-        return create_response(
-                client,
-                "200 OK",
-                {
-                    "Content-Type": "text/plain",
-                    "Content-Length": len(headers["User-Agent"]),
-                },
-                headers["User-Agent"].encode(),
-            )
-    client.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-    client.close()
-    return
+        elif method.upper() == "POST":
+            p.write_bytes(body.encode())
+            writer.write(make_response(201, response_headers))
+        else:
+            writer.write(make_response(404, response_headers))
+        stderr(f"[OUT] file {path}")
+    else:
+        writer.write(make_response(404, response_headers, ""))
+        stderr(f"[OUT] 404")
 
-def main():
+    await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+async def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory")
+    parser.add_argument("--directory", default=".")
     args = parser.parse_args()
 
-    if "directory" in args:
-        global FILE_DIR
-        FILE_DIR = args.directory
-    server_socket = socket.create_server(("localhost", 4221))  # , reuse_port=True
+    GLOBALS["DIR"] = args.directory
 
-    while True:
-        client, _ = server_socket.accept()  # wait for client
-        threading.Thread(target=handle_client, args=(client,)).start()
+    server = await asyncio.start_server(handle_connection, "localhost", 4221)
+    async with server:
+        stderr("Starting server...")
+        stderr(f"--directory {GLOBALS['DIR']}")
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
